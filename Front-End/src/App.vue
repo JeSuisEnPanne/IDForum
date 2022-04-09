7d86151175d37e69d57963e777c531af8456875c
Steve Lheureux <steve.lheureux@yahoo.fr>
2022-04-09 15:34:51 +0200

    Début du projet 7 front-end et back-end

---
+++ Front-End/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <InscritsIndex></InscritsIndex>
+</template>
+
+<script>
+import InscritsIndex from './components/InscritsIndex.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InscritsIndex
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
